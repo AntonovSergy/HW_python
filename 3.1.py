1553a9b4c2d5e4d7519798439204868a02e3175a
Sergey <sergey.antonov.2020.02.02@mail.ru>
2021-11-16 13:02:18 +0200

    add file 3.1.py

---
+++ 3.1.py
@@ -0,0 +1,17 @@
+my_dict = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+
+def num_translate(enter):
+    return my_dict.get(enter)
+
+print(num_translate("ten"))
